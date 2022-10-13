4877a3f5801ef386daffd703e8af15fa475edeb5
Ekko <2487242076@qq.com>
2022-10-13 17:24:33 +0800

    2022.10.13

--- main.go
+++ main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"gin_consult/config"
-	//"gin_consult/routers"
+	"gin_consult/routers"
 )
 
 func main() {
 	// Ek1+Ep1==Ek2+Ep2
 	config.Init()
-	//r := routes.NewRouter()
-	//_=r.Run(conf.HttpPort)
+	r := routers.NewRouter()
+	_=r.Run(config.HttpPort)
 }
